eb79f07e4bc250f412a4bef1529a49d7e0dad9a5
MaBlock <2985861993@qq.com>
2026-01-14 08:22:25 +0800

    修复gcc13 vcpkg编译mujico时的报错

--- triplets/x64-linux-static.cmake
+++ triplets/x64-linux-static.cmake
@@ -3,6 +3,9 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
+set(VCPKG_C_FLAGS "-Wno-error=format-truncation")
+set(VCPKG_CXX_FLAGS "-Wno-error=format-truncation")
+
 if(PORT MATCHES "mujoco")
     set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif()
