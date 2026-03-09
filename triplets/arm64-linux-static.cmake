045c26eae9e1791c8e5f21d552facee5bbe9dee3
MaBlock <2985861993@qq.com>
2026-03-09 18:04:09 +0800

    添加arm64 triplet文件

---
+++ triplets/arm64-linux-static.cmake
@@ -0,0 +1,11 @@
+set(VCPKG_TARGET_ARCHITECTURE arm64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+set(VCPKG_C_FLAGS "-Wno-error=format-truncation")
+set(VCPKG_CXX_FLAGS "-Wno-error=format-truncation")
+
+if(PORT MATCHES "mujoco|libsystemd")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
