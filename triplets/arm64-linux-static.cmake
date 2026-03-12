5bbf62b12406dc26b2d51035e68e5764865308cc
bighj256 <dongjiajundjj2006@qq.com>
2026-03-12 08:15:52 +0800

    修复arm64 cicd

--- triplets/arm64-linux-static.cmake
+++ triplets/arm64-linux-static.cmake
@@ -3,8 +3,8 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
-set(VCPKG_C_FLAGS "-Wno-error=format-truncation -Wa,-march=armv8.3-a")
-set(VCPKG_CXX_FLAGS "-Wno-error=format-truncation -Wa,-march=armv8.3-a")
+set(VCPKG_C_FLAGS "-Wno-error=format-truncation -march=armv8.3-a")
+set(VCPKG_CXX_FLAGS "-Wno-error=format-truncation -march=armv8.3-a")
 
 if(PORT MATCHES "mujoco|libsystemd")
     set(VCPKG_LIBRARY_LINKAGE dynamic)
